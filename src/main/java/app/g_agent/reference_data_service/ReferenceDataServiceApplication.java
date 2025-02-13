c1b371f30042f31433f55bd6f4accbe66efc67d2
Ndiithi <duncanndiithi@yahoo.com>
2025-02-13 19:58:10 +0300

    initial commit

---
+++ src/main/java/app/g_agent/reference_data_service/ReferenceDataServiceApplication.java
@@ -0,0 +1,13 @@
+package app.g_agent.reference_data_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferenceDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReferenceDataServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/g_agent/reference_data_service/ReferenceDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package app.g_agent.reference_data_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReferenceDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
