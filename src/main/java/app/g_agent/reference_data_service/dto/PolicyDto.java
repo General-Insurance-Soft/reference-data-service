d6aabf2035bc4160224961793e4afc0d6b454106
Ndiithi <duncanndiithi@yahoo.com>
2025-02-17 18:11:04 +0300

    add dtos

---
+++ src/main/java/app/g_agent/reference_data_service/dto/PolicyDto.java
@@ -0,0 +1,68 @@
+package app.g_agent.reference_data_service.dto;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public class PolicyDto {
+
+    private Long id;
+
+    @NotBlank(message = "Name is required")
+    @NotNull(message = "Name is required")
+    private String name;
+
+    @JsonProperty(value = "created_at")
+    private LocalDateTime createdAt;
+
+    @JsonProperty(value = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @JsonProperty(value = "updated_by")
+    private Long updatedBy;
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(Long updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+}
